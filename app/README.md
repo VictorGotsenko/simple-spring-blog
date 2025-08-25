4de51a088660ac5fbae9ea399ab11a4f3f87c97d
VictorGotsenko <victor.gotsenko@gmail.com>
2025-08-25 19:50:24 +0500

    Refactor README.md. Move to root folder

--- app/README.md
+++
@@ -1,5 +0,0 @@
-## Sandbox-project using Spring Boot
-
-### Полезные ссылки
-
-* [Начало работы со Spring Boot](https://spring.io/quickstart)
--- app/src/main/java/spring/blog/Application.java
+++ app/src/main/java/spring/blog/Application.java
@@ -1,4 +1,14 @@
 package spring.blog;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
