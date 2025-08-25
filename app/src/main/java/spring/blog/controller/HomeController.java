a58c70b3261d5d4f2cc16c580914068e54dceac7
VictorGotsenko <victor.gotsenko@gmail.com>
2025-08-25 20:03:13 +0500

    Add path About

--- app/src/main/java/spring/blog/controller/HomeController.java
+++ app/src/main/java/spring/blog/controller/HomeController.java
@@ -5,9 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
     @GetMapping("/")
     public String home() {
         return "Добро пожаловать в Hexlet Spring Blog!";
     }
+
+    //у блога есть отдельная страница «О проекте». less 03
+    @GetMapping("/about")
+    public String about() {
+        return "This is simple Spring blog!";
+    }
+
 }
